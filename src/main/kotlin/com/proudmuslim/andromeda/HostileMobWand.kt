9652a28ec62218f85a1542a96139702274f91e9c
Proudmuslim <proudmuslimos@protonmail.ch>
2020-12-04 14:10:46 -0800

    Add sounds to HostileMobWand

--- src/main/kotlin/com/proudmuslim/andromeda/HostileMobWand.kt
+++ src/main/kotlin/com/proudmuslim/andromeda/HostileMobWand.kt
@@ -7,12 +7,20 @@ import net.minecraft.item.ItemStack
 import net.minecraft.world.World
 import net.minecraft.util.Hand
 import net.minecraft.item.Item
+import net.minecraft.sound.SoundCategory
+import net.minecraft.sound.SoundEvents
 
 class HostileMobWand(settings: Settings): Item(settings) {
 
-    override fun use(world: World?, playerEntity: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
-        world?.setMobSpawnOptions(hand == Hand.OFF_HAND, true)
+    override fun use(world: World, playerEntity: PlayerEntity?, hand: Hand?): TypedActionResult<ItemStack> {
+        world.setMobSpawnOptions(hand == Hand.OFF_HAND, true)
         playerEntity?.sendMessage(LiteralText("Set hostile mab spawning to " + (hand == Hand.OFF_HAND)), false)
+
+        when(!world.isClient) {
+            true -> {
+                world.playSound(null, playerEntity?.blockPos, SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER, SoundCategory.WEATHER, 1f, 1f)
+            }
+        }
 
         return super.use(world, playerEntity, hand)
     }
